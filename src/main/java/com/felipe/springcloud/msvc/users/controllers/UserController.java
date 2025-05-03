9ce0d9b9da33bc9af5fbe6a4c64c3979e17e77be
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-05-03 11:37:06 -0400

    handling password encrypt on the servide and setting the default role to new users

--- src/main/java/com/felipe/springcloud/msvc/users/controllers/UserController.java
+++ src/main/java/com/felipe/springcloud/msvc/users/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.felipe.springcloud.msvc.users.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.felipe.springcloud.msvc.users.entities.User;
@@ -16,12 +15,9 @@ public class UserController {
 
     @Autowired
     private IUserService userService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser = userService.save(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
--- src/main/java/com/felipe/springcloud/msvc/users/services/UserService.java
+++ src/main/java/com/felipe/springcloud/msvc/users/services/UserService.java
@@ -1,12 +1,17 @@
 package com.felipe.springcloud.msvc.users.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.felipe.springcloud.msvc.users.entities.Role;
 import com.felipe.springcloud.msvc.users.entities.User;
+import com.felipe.springcloud.msvc.users.repositories.RoleRepository;
 import com.felipe.springcloud.msvc.users.repositories.UserRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,6 +19,12 @@ public class UserService implements IUserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Transactional(readOnly = true)
     public Optional<User> findById(Long id) {
@@ -32,6 +43,13 @@ public class UserService implements IUserService {
 
     @Transactional
     public User save(User user) {
+        // Encrypt the password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        List<Role> roles = new ArrayList<>();
+        Optional<Role> role = roleRepository.findByName("ROLE_USER");
+        role.ifPresent(x -> roles.add(x));
+        user.setRoles(roles);
         return userRepository.save(user);
     }
 

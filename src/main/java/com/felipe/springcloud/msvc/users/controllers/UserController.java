7f0d27e9a39ea61443dcafce2176c7d7d887a499
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-05-03 12:34:45 -0400

    implementing clean code on UserController

--- src/main/java/com/felipe/springcloud/msvc/users/controllers/UserController.java
+++ src/main/java/com/felipe/springcloud/msvc/users/controllers/UserController.java
@@ -18,8 +18,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        User savedUser = userService.save(user);
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
@@ -33,15 +32,13 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        Optional<User> user = userService.findById(id);
-        return user.map(ResponseEntity::ok)
+        return userService.findById(id).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/username/{username}")
     public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
-        Optional<User> user = userService.findByUsername(username);
-        return user.map(ResponseEntity::ok)
+        return userService.findByUsername(username).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
